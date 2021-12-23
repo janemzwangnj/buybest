cbd4164ceeae4c996fcfa6e130e43a75b6df9a6f
janemzwangnj <janemzwangnj@gmail.com>
2021-12-23 12:20:40 -0500

    update gitReviewById res.status(404) to res.status(200)

--- controllers/index.js
+++ controllers/index.js
@@ -96,7 +96,7 @@ const getReviewsById = async (req, res) => {
     if (reviews) {
       return res.status(200).json({ reviews });
     }
-    return res.status(404).send('Review with the specified ID does not exists');
+    return res.status(200).send('Review with the specified ID does not exists');
   } catch (error) {
     return res.status(500).send(error.message);
   }
